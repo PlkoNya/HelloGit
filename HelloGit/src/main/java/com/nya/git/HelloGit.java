4b9b62d9725eccf2c1fb28ebec39a15c79107bc4
PlkoNya <1913698293@qq.com>
2020-10-21 19:58:44 +0800

    create branch

--- HelloGit/src/main/java/com/nya/git/HelloGit.java
+++ HelloGit/src/main/java/com/nya/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Update Hello Git");
+        System.out.println("Test Branch");
     }
 }
